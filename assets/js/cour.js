606132f360f1f4ffc7254e3e1b8bff60e11417b0
Nate Pond <nathaniel.p.pond@gmail.com>
2017-04-09 08:10:49 -0400

    transitions

--- assets/js/cour.js
+++ assets/js/cour.js
@@ -21,3 +21,58 @@
 // }
 
 // jqbubbleColorize()
+
+
+$(document).ready(function() {
+  // going out page on ready functions //
+  $(".search").hide();
+  $(".search2").hide();
+  $("#panel1-results").hide();
+  $("#panel2-results").hide();
+  // $(".bubbleContainer").hide();
+
+
+
+})
+
+function clickFunctionsForGoingOutPage() {
+  $(".concerts-sports-click").click(function() {
+    $("#first-info-panel2").hide();
+    $(".search2").fadeIn(2000);
+  });
+  $(".left-side-click").click(function() {
+    $("#first-info-panel1").hide();
+    $(".search").fadeIn(2000);
+  });
+  $("#coffee-click").click(function() {
+    $("#first-info-panel1").hide();
+    $("#panel1-results").fadeIn(2000);
+  });
+  $("#restaurants-click").click(function() {
+    $("#first-info-panel1").hide();
+    $("#panel1-results").fadeIn(2000);
+  });
+  $("#libations-click").click(function() {
+    $("#first-info-panel1").hide();
+    $("#panel1-results").fadeIn(2000);
+  });
+  $("#theater-click").click(function() {
+    $("#first-info-panel2").hide();
+    $("#panel2-results").fadeIn(2000);
+  });
+  $("#museum-click").click(function() {
+    $("#first-info-panel2").hide();
+    $("#panel2-results").fadeIn(2000);
+  });
+  $("#back-btn-panel1").click(function() {
+    $("#panel1-results").hide();
+    $("#first-info-panel1").fadeIn(2000);
+  })
+  $("#back-btn-panel2").click(function() {
+    $(".search2").hide();
+    $("#first-info-panel2").fadeIn(2000);
+  })
+
+
+};
+clickFunctionsForGoingOutPage();
